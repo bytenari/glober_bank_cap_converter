3382ca8f23e67560fc8e87948728359461e3f53b
bytenari <103120984+bytenari@users.noreply.github.com>
2024-08-30 13:00:10 +0900

    Update banks_project.py

--- banks_project.py
+++ banks_project.py
@@ -53,41 +53,66 @@ def transform(df, csv_path):
     information, and adds three columns to the data frame, each
     containing the transformed version of Market Cap column to
     respective currencies'''
-    attribute_list = ['EUR', 'GBP', 'INR']
-    df = pd.read_csv(csv_path, names = attribute_list)
-    exchange_rate = dataframe.set_index('Currency').to_dict('Rate')]
-    df['MC_GBP_Billion'] = [np.round(x*exchange_rate['GBP'],2) for x in df['MC_USD_Billion']]
-    df['MC_EUR_Billion'] = [np.round(x*exchange_rate['EUR'],2) for x in df['MC_USD_Billion']]
-    df['MC_INR_Billion'] = [np.round(x*exchange_rate['INR'],2) for x in df['MC_USD_Billion']]
- 
+    rate_table = 'Exchange_rate'
+    #= ['EUR', 'GBP', 'INR']
+    ex_rate_df = pd.read_csv(csv_path)
+    ex_rate_dict = ex_rate_df.set_index('Currency').to_dict()['Rate']
+
+    df["MC_GBP_Billion"] = [np.round(x*ex_rate_dict["GBP"],2) for x in df["MC_USD_Billion"]]
+    df["MC_EUR_Billion"] = [np.round(x*ex_rate_dict["EUR"],2) for x in df["MC_USD_Billion"]]
+    df["MC_INR_Billion"] = [np.round(x*ex_rate_dict["INR"],2) for x in df["MC_USD_Billion"]]
+
     return df
 
 def load_to_csv(df, output_path):
     ''' This function saves the final data frame as a CSV file in
     the provided path. Function returns nothing.'''
+    df.to_csv(output_path)
 
 def load_to_db(df, sql_connection, table_name):
     ''' This function saves the final data frame to a database
     table with the provided name. Function returns nothing.'''
+    df.to_sql(table_name, sql_connection, if_exists='replace', index=False)
 
 def run_query(query_statement, sql_connection):
     ''' This function runs the query on the database table and
     prints the output on the terminal. Function returns nothing. '''
+    print(query_statement)
+    query_output = pd.read_sql(query_statement, sql_connection)
+    print(query_output)
 
 ''' Here, you define the required entities and call the relevant
 functions in the correct order to complete the project. Note that this
 portion is not inside any function.'''
 url = 'https://web.archive.org/web/20230908091635/https://en.wikipedia.org/wiki/List_of_largest_banks'
-exrate_csv_path = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMSkillsNetwork-PY0221EN-Coursera/labs/v2/exchange_rate.csv'
+csv_path = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMSkillsNetwork-PY0221EN-Coursera/labs/v2/exchange_rate.csv'
 table_attribs = ["Name", "MC_USD_Billion"]
 db_name = 'Banks.db'
 table_name = 'Largest_banks'
-csv_path = './Largest_banks_data.csv'
+output_path = './Largest_banks_data.csv'
     
 log_progress('Preliminaries complete. Initiating ETL process')
 df = extract(url, table_attribs)
 print(df)
 
 log_progress('Data extraction complete. Initiating Transformation process')
-df = transform(df, exrate_csv_path)
+df = transform(df, csv_path)
 print(df)
+
+load_to_csv(df, output_path)
+log_progress('Data saved to CSV file')
+
+sql_connection = sqlite3.connect('Banks.db')
+log_progress('SQL Connection initiated.')
+load_to_db(df, sql_connection, table_name)
+log_progress('Data loaded to Database as table.')
+
+log_progress('Running the query')
+query_statement = f"SELECT * from {table_name}"
+run_query(query_statement, sql_connection)
+query_statement = f"SELECT AVG(MC_GBP_Billion) from {table_name}"
+run_query(query_statement, sql_connection)
+query_statement = f"SELECT Name from {table_name} LIMIT 5"
+run_query(query_statement, sql_connection)
+log_progress('Process Complete.')
+sql_connection.close()
